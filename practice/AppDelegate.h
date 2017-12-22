a0c5867bdacd3446d2b4b6fef0e9fd24fde6b995
wenhua <xudacheng_tomous@163.com>
2017-12-22 14:19:05 +0800

    Initial Commit

---
+++ practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  practice
+//
+//  Created by wenhua on 2017/12/22.
+//  Copyright © 2017年 wenhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ practice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practice
+//
+//  Created by wenhua on 2017/12/22.
+//  Copyright © 2017年 wenhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
